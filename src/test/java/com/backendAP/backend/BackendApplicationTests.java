03b09a776a5a9672a57068a8cf2f63ccef681a39
Antoniojjcastro <antoniojjcastro@gmail.com>
2022-11-03 14:01:31 -0300

    first commit

---
+++ src/test/java/com/backendAP/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendAP.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
